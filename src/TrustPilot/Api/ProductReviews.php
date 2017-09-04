bddc7b2dec8148fd394a5e7919c45490bc6a8b37
Nei Santos <ns@amp.co>
2017-09-04 17:32:54 +0100

    Fix phpdocs

--- src/TrustPilot/Api/ProductReviews.php
+++ src/TrustPilot/Api/ProductReviews.php
@@ -19,18 +19,18 @@ use Carbon\Carbon;
 use TrustPilot\TrustPilot;
 
 class ProductReviews extends AbstractApi{
-  
+
     /**
      * Create product review invitation link
      * https://developers.trustpilot.com/product-reviews-api#create-product-review-invitation-link
-     * 
+     *
      *
      * @param  string, array
-     * @return array
+     * @return \stdClass
      */
     public function createInvitationLink($businessUnitId, $data)
     {
-        $data['locale'] = isset($data['local']) ?: 'en-US' ;
+	$data['locale'] = isset($data['locale']) ? $data['locale'] : 'en-US' ;
 
         return json_decode(
             $this->api->post('private/product-reviews/business-units/'. $businessUnitId .'/invitation-links',array('json' => $data)));
@@ -39,23 +39,22 @@ class ProductReviews extends AbstractApi{
     /**
      * Get private product reviews
      * https://developers.trustpilot.com/product-reviews-api#get-private-product-reviewsnk
-     * 
+     *
      *
      * @param  string, array
-     * @return array
+     * @return \stdClass
      */
     public function getPrivateReviews($businessUnitId, $data)
     {
-
         return json_decode(
             $this->api->get('private/product-reviews/business-units/'. $businessUnitId .'/reviews',
-                ['query' => 
+                ['query' =>
                     [
                        'page' => $data['page'],
-                       'perPage' => $data['perPage'], 
-                       'sku' => $data['sku'], 
-                       'language' => $data['language'], 
-                       'state' => $data['state'], 
+                       'perPage' => $data['perPage'],
+                       'sku' => $data['sku'],
+                       'language' => $data['language'],
+                       'state' => $data['state'],
                     ]
                 ]
             ));
@@ -64,20 +63,19 @@ class ProductReviews extends AbstractApi{
     /**
      * Get private product reviews
      * https://developers.trustpilot.com/product-reviews-api#get-product-reviews-summaries-list
-     * 
+     *
      *
      * @param  string, array
-     * @return array
+     * @return \stdClass
      */
     public function getReviewsSummaries($businessUnitId , $data)
     {
-
         return json_decode(
             $this->api->get('private/product-reviews/business-units/'. $businessUnitId .'/summaries',
-                ['query' => 
+                ['query' =>
                     [
                        'page' => $data['page'],
-                       'perPage' => $data['perPage'] 
+                       'perPage' => $data['perPage']
                     ]
                 ]
             ));
@@ -86,14 +84,13 @@ class ProductReviews extends AbstractApi{
     /**
      * Get private product review
      * https://developers.trustpilot.com/product-reviews-api#get-private-product-reviews-list
-     * 
      *
-     * @param  string
-     * @return array
+     *
+     * @param string
+     * @return \stdClass
      */
     public function getPrivateReview($reviewId)
     {
-
         return json_decode(
             $this->api->get('private/product-reviews/'. $reviewId));
     }
@@ -101,10 +98,10 @@ class ProductReviews extends AbstractApi{
     /**
      * Create product review conversation
      * https://developers.trustpilot.com/product-reviews-api#create-product-review-conversation
-     * 
      *
-     * @param  string
-     * @return array
+     *
+     * @param string
+     * @return string
      */
     public function createReviewConversation($reviewId)
     {
@@ -117,27 +114,26 @@ class ProductReviews extends AbstractApi{
     /**
      * Get product reviews summary
      * https://developers.trustpilot.com/product-reviews-api#get-product-reviews-summaryist
-     * 
      *
-     * @param  string, array
-     * @return array
+     *
+     * @param string|array
+     * @return \stdClass
      */
     public function getReviewsSummary($businessUnitId, $data)
     {
-
         return json_decode(
             $this->api->get('private/product-reviews/business-units/'. $businessUnitId,
-                ['query' => 
+                ['query' =>
                     [
                        'page' => $data['page'],
-                       'perPage' => $data['perPage'] 
+                       'perPage' => $data['perPage']
                     ]
                 ]
             ));
     }
 
-    // Incomplete Issue right now is they are mixing some call in the same area 
-    // With oauth token and api key, so need to divide this class in 2 to 
+    // Incomplete Issue right now is they are mixing some call in the same area
+    // With oauth token and api key, so need to divide this class in 2 to
     // Support the api keys call as well
-    
+
 }
